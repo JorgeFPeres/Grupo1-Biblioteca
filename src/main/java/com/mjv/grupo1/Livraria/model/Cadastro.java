e1076b593ec593c23ebb8146ceeec6af156ca508
Jorge Peres <jorge-peres@live.com>
2021-03-29 16:33:15 -0300

    Novas alteracoes

--- src/main/java/com/mjv/grupo1/Livraria/model/Cadastro.java
+++ src/main/java/com/mjv/grupo1/Livraria/model/Cadastro.java
@@ -2,4 +2,68 @@ package com.mjv.grupo1.Livraria.model;
 
 public class Cadastro {
 
+	private Integer id;
+	private String cpf;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String login;
+	private String senha;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
