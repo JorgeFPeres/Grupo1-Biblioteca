6aa63092c7f9d5b78020b7c09b4c3e7578520beb
Leonardo Richard <leopm.jus@gmail.com>
2021-04-03 13:33:31 -0300

    Update LocacaoController.java

--- src/main/java/com/mjv/grupo1/livraria/controller/LocacaoController.java
+++ src/main/java/com/mjv/grupo1/livraria/controller/LocacaoController.java
@@ -1,15 +1,24 @@
 package com.mjv.grupo1.livraria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mjv.grupo1.livraria.dto.LocacaoDto;
+import com.mjv.grupo1.livraria.services.LocacaoService;
 
 @RestController
 @RequestMapping(path = "/locacoes")
 public class LocacaoController {
 	
+	@Autowired
+	private LocacaoService service;
 	
+	@PostMapping
+	public void post(@RequestBody LocacaoDto body) {
+		service.gerarLocacao(body);
+	}
 
-	
-	
 }
-
