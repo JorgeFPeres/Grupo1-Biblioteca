96d7954a1569a55a9c0e5c9f9dc0596f851f95ef
Davi <davi.mendonca@outlook.com>
2021-04-03 21:33:27 -0300

    Update ViaCep

---
+++ src/main/java/com/mjv/grupo1/livraria/controller/ViaCepController.java
@@ -0,0 +1,25 @@
+package com.mjv.grupo1.livraria.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.mjv.grupo1.livraria.model.Endereco;
+
+@RestController
+public class ViaCepController {
+
+	@GetMapping(value = "/getCep/{cep}")
+	public Endereco obterEndereco(@PathVariable(name = "cep") String cep) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String uri = "http://viacep.com.br/ws/{cep}/json/";
+
+		Endereco endereco = restTemplate.getForObject(uri, Endereco.class, cep);
+
+		return endereco;
+	}
+
+}
--- src/main/java/com/mjv/grupo1/livraria/services/CadastroServices.java
+++ src/main/java/com/mjv/grupo1/livraria/services/CadastroServices.java
@@ -1,13 +1,12 @@
 package com.mjv.grupo1.livraria.services;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.mjv.grupo1.livraria.controller.ViaCepController;
 import com.mjv.grupo1.livraria.dto.CadastroDto;
 import com.mjv.grupo1.livraria.model.Cadastro;
 import com.mjv.grupo1.livraria.model.Endereco;
@@ -16,49 +15,45 @@ import com.mjv.grupo1.livraria.repository.CadastroRepository;
 @Service
 //@RequiredArgsConstructor
 public class CadastroServices {
-	
-	//@Autowired
-	//private ViaCEPClient viaCepClient;
+
+	@Autowired
+	private ViaCepController viaCepCtrl;
 	
 	@Autowired
 	private PasswordEncoder encoder;
-	
+
 	@Autowired
 	private CadastroRepository cadastroRepository;
-	
-	
-	public List<Cadastro> listAll(){
-        return cadastroRepository.findAll();
-    }
-	
-	public Cadastro findById(Integer id){
-        return cadastroRepository.findById(id).orElse(null);
-    }
+
+	public List<Cadastro> listAll() {
+		return cadastroRepository.findAll();
+	}
+
+	public Cadastro findById(Integer id) {
+		return cadastroRepository.findById(id).orElse(null);
+	}
+
 	public Cadastro save(CadastroDto dto) {
-		//ResteTemplate
 		Cadastro cadastro = new Cadastro();
 		cadastro.setCpf(dto.getCpf());
 		cadastro.setEmail(dto.getEmail());
 		cadastro.setLogin(dto.getLogin());
 		cadastro.setNome(dto.getNome());
 		cadastro.setTelefone(dto.getTelefone());
-		//Endereco end = viaCepClient.buscaEnderecoPor(dto.getCep());
-		
-		RestTemplate template = new RestTemplate();
-        Endereco end= template.getForObject("https://viacep.com.br/ws/{cep}/json",Endereco.class, dto.getCep());
-        
-		cadastro.setEndereco(end);
+		Endereco endereco = viaCepCtrl.obterEndereco(dto.getCep());
+		cadastro.setEndereco(endereco);
 		return save(cadastro);
 	}
+
 	public Cadastro save(Cadastro cadastro) {
-		String senhaCriptografada =encoder.encode(cadastro.getLogin().getSenha());
+		String senhaCriptografada = encoder.encode(cadastro.getLogin().getSenha());
 		cadastro.getLogin().setSenha(senhaCriptografada);
-		
+
 		return cadastroRepository.save(cadastro);
 	}
-	
+
 	public void delete(Integer id) {
-        cadastroRepository.delete(findById(id));
-    }
+		cadastroRepository.delete(findById(id));
+	}
 
 }
